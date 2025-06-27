26bce59f88cbec2adf125c9ac04e87b3a6ea5624
Moustafa Othman <105397454+MustafaOt7man@users.noreply.github.com>
2025-06-27 19:39:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -66,4 +66,4 @@ def predict():
         }), 500
 
 if __name__ == "__main__":
- app.run(debug=True, port=5000, use_reloader=False)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
