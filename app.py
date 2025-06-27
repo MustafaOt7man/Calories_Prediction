f31606c14f2957dc253c21f481fe78ef3bcfe93e
Moustafa Othman <105397454+MustafaOt7man@users.noreply.github.com>
2025-06-27 19:44:54 +0300

    Update app.py
    updated

--- app.py
+++ app.py
@@ -66,4 +66,5 @@ def predict():
         }), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get("PORT", 5000))  # Railway will inject PORT
+    app.run(host="0.0.0.0", port=port)
